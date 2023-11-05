2c11652672c5f20323bd9001843bf565fbbb7542
Oguz Kanda <exporler95@gmail.com>
2023-11-05 15:56:49 +0300

    Initial Commit

---
+++ Bitirme Projesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bitirme Projesi
+//
+//  Created by Oğuz Kanda on 5.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
